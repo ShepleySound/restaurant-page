492b5190928e7634183022dccfac843c0aa0c357
ShepleySound <shepleysound@gmail.com>
2022-01-01 22:54:08 -0800

    Change footer date.

--- src/load-footer.js
+++ src/load-footer.js
@@ -2,7 +2,7 @@ export default function loadFooter() {
     const footer = document.createElement('footer')
     const footerText = document.createElement('a')
     footerText.setAttribute('id', 'copyright')
-    footerText.innerText = "Robert Shepley 2021"
+    footerText.innerText = "Robert Shepley 2022"
 
     footer.append(footerText)
     return footer
